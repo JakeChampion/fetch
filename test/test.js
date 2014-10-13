f95e98c8c533dd6073c62a56ff8a7889c19751c6
David Graham <david.malcom.graham@gmail.com>
2014-10-13 14:23:45 -0600

    Add json tests.

--- test/test.js
+++ test/test.js
@@ -4,6 +4,12 @@ MockXHR.responses = {
   },
   '/boom': function(xhr) {
     xhr.respond(500, 'boom')
+  },
+  '/json': function(xhr) {
+    xhr.respond(200, JSON.stringify({name: 'Hubot', login: 'hubot'}))
+  },
+  '/json-error': function(xhr) {
+    xhr.respond(200, 'not json {')
   }
 }
 
@@ -13,6 +19,26 @@ asyncTest('populates response body', 2, function() {
   fetch('/hello').then(function(response) {
     equal(response.status, 200)
     equal(response.body, 'hi')
+    start()
+  })
+})
+
+asyncTest('parses json response', 2, function() {
+  fetch('/json').then(function(response) {
+    return response.json()
+  }).then(function(json) {
+    equal(json.name, 'Hubot')
+    equal(json.login, 'hubot')
+    start()
+  })
+})
+
+asyncTest('handles json parse error', 2, function() {
+  fetch('/json-error').then(function(response) {
+    return response.json()
+  }).catch(function(error) {
+    ok(error instanceof Error, 'JSON exception is an Error instance')
+    ok(error.message, 'JSON exception has an error message')
     start()
   })
 })
