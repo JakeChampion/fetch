b3b1fefa5096c1978c552fcc172f6484b2c64e9b
knowledgecode <knowledgecode@gmail.com>
2014-12-24 21:29:40 +0900

    Web Workers support

--- fetch.js
+++ fetch.js
@@ -1,7 +1,7 @@
-(function() {
+(function(global) {
   'use strict';
 
-  if (window.fetch) {
+  if (global.fetch) {
     return
   }
 
@@ -203,7 +203,7 @@
 
   Body.call(Response.prototype)
 
-  window.fetch = function (url, options) {
+  global.fetch = function (url, options) {
     return new Request(url, options).fetch()
   }
-})();
+})(this);
