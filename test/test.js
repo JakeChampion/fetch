ecd8600932b0d8495d646df0d6fa74874cd57713
Joshua Peek <josh@joshpeek.com>
2015-01-10 17:56:15 -0800

    Group tests

--- test/test.js
+++ test/test.js
@@ -1,33 +1,3 @@
-test('populates response body', function() {
-  return fetch('/hello').then(function(response) {
-    assert.equal(response.status, 200)
-    return response.text()
-  }).then(function(body) {
-    assert.equal(body, 'hi')
-  })
-})
-
-test('sends request headers', function() {
-  return fetch('/request', {
-    headers: {
-      'Accept': 'application/json',
-      'X-Test': '42'
-    }
-  }).then(function(response) {
-    return response.json()
-  }).then(function(json) {
-    assert.equal(json.headers['accept'], 'application/json')
-    assert.equal(json.headers['x-test'], '42')
-  })
-})
-
-test('parses response headers', function() {
-  return fetch('/headers?' + new Date().getTime()).then(function(response) {
-    assert.equal(response.headers.get('Date'), 'Mon, 13 Oct 2014 21:02:27 GMT')
-    assert.equal(response.headers.get('Content-Type'), 'text/html; charset=utf-8')
-  })
-})
-
 test('resolves promise on 500 error', function() {
   return fetch('/boom').then(function(response) {
     assert.equal(response.status, 500)
@@ -45,115 +15,159 @@ test('rejects promise for network error', function() {
   })
 })
 
-test('handles 204 No Content response', function() {
-  return fetch('/empty').then(function(response) {
-    assert.equal(response.status, 204)
-    return response.text()
-  }).then(function(body) {
-    assert.equal(body, '')
-  })
-})
-
-test('resolves text promise', function() {
-  return fetch('/hello').then(function(response) {
-    return response.text()
-  }).then(function(text) {
-    assert.equal(text, 'hi')
-  })
-})
-
-test('parses json response', function() {
-  return fetch('/json').then(function(response) {
-    return response.json()
-  }).then(function(json) {
-    assert.equal(json.name, 'Hubot')
-    assert.equal(json.login, 'hubot')
-  })
-})
-
-test('handles json parse error', function() {
-  return fetch('/json-error').then(function(response) {
-    return response.json()
-  }).catch(function(error) {
-    assert(error instanceof Error, 'JSON exception is an Error instance')
-    assert(error.message, 'JSON exception has an error message')
-  })
-})
-
-;(Response.prototype.blob ? suite : suite.skip)('Blob', function() {
-  test('resolves blob promise', function() {
-    return fetch('/hello').then(function(response) {
-      return response.blob()
-    }).then(function(blob) {
-      assert(blob instanceof Blob, 'blob is a Blob instance')
-      assert.equal(blob.size, 2)
-    })
-  })
-
-  test('rejects blob promise after body is consumed', function() {
-    return fetch('/hello').then(function(response) {
-      assert(response.blob, 'Body does not implement blob')
-      response.blob()
-      return response.blob()
-    }).catch(function(error) {
-      assert(error instanceof TypeError, 'Promise rejected after body consumed')
-    })
-  })
-})
-
-;(Response.prototype.formData ? suite : suite.skip)('FormData', function() {
-  test('post sets content-type header', function() {
+// https://fetch.spec.whatwg.org/#request-class
+suite('Request', function() {
+  test('sends request headers', function() {
     return fetch('/request', {
-      method: 'post',
-      body: new FormData()
+      headers: {
+        'Accept': 'application/json',
+        'X-Test': '42'
+      }
     }).then(function(response) {
       return response.json()
     }).then(function(json) {
-      assert.equal(json.method, 'POST')
-      assert(/^multipart\/form-data;/.test(json.headers['content-type']))
-    })
-  })
-
-  test('rejects formData promise after body is consumed', function() {
-    return fetch('/json').then(function(response) {
-      assert(response.formData, 'Body does not implement formData')
-      response.formData()
-      return response.formData()
-    }).catch(function(error) {
-      assert(error instanceof TypeError, 'Promise rejected after body consumed')
-    })
-  })
-
-  test('parses form encoded response', function() {
-    return fetch('/form').then(function(response) {
-      return response.formData()
-    }).then(function(form) {
-      assert(form instanceof FormData, 'Parsed a FormData object')
+      assert.equal(json.headers['accept'], 'application/json')
+      assert.equal(json.headers['x-test'], '42')
     })
   })
 })
 
-test('rejects json promise after body is consumed', function() {
-  return fetch('/json').then(function(response) {
-    assert(response.json, 'Body does not implement json')
-    response.json()
-    return response.json()
-  }).catch(function(error) {
-    assert(error instanceof TypeError, 'Promise rejected after body consumed')
+// https://fetch.spec.whatwg.org/#response-class
+suite('Response', function() {
+  test('populates response body', function() {
+    return fetch('/hello').then(function(response) {
+      assert.equal(response.status, 200)
+      return response.text()
+    }).then(function(body) {
+      assert.equal(body, 'hi')
+    })
+  })
+
+  test('parses response headers', function() {
+    return fetch('/headers?' + new Date().getTime()).then(function(response) {
+      assert.equal(response.headers.get('Date'), 'Mon, 13 Oct 2014 21:02:27 GMT')
+      assert.equal(response.headers.get('Content-Type'), 'text/html; charset=utf-8')
+    })
   })
 })
 
-test('rejects text promise after body is consumed', function() {
-  return fetch('/hello').then(function(response) {
-    assert(response.text, 'Body does not implement text')
-    response.text()
-    return response.text()
-  }).catch(function(error) {
-    assert(error instanceof TypeError, 'Promise rejected after body consumed')
+// https://fetch.spec.whatwg.org/#body-mixin
+suite('Body mixin', function() {
+  ;(Response.prototype.blob ? suite : suite.skip)('blob', function() {
+    test('resolves blob promise', function() {
+      return fetch('/hello').then(function(response) {
+        return response.blob()
+      }).then(function(blob) {
+        assert(blob instanceof Blob, 'blob is a Blob instance')
+        assert.equal(blob.size, 2)
+      })
+    })
+
+    test('rejects blob promise after body is consumed', function() {
+      return fetch('/hello').then(function(response) {
+        assert(response.blob, 'Body does not implement blob')
+        response.blob()
+        return response.blob()
+      }).catch(function(error) {
+        assert(error instanceof TypeError, 'Promise rejected after body consumed')
+      })
+    })
+  })
+
+  ;(Response.prototype.formData ? suite : suite.skip)('formData', function() {
+    test('post sets content-type header', function() {
+      return fetch('/request', {
+        method: 'post',
+        body: new FormData()
+      }).then(function(response) {
+        return response.json()
+      }).then(function(json) {
+        assert.equal(json.method, 'POST')
+        assert(/^multipart\/form-data;/.test(json.headers['content-type']))
+      })
+    })
+
+    test('rejects formData promise after body is consumed', function() {
+      return fetch('/json').then(function(response) {
+        assert(response.formData, 'Body does not implement formData')
+        response.formData()
+        return response.formData()
+      }).catch(function(error) {
+        assert(error instanceof TypeError, 'Promise rejected after body consumed')
+      })
+    })
+
+    test('parses form encoded response', function() {
+      return fetch('/form').then(function(response) {
+        return response.formData()
+      }).then(function(form) {
+        assert(form instanceof FormData, 'Parsed a FormData object')
+      })
+    })
+  })
+
+  suite('json', function() {
+    test('parses json response', function() {
+      return fetch('/json').then(function(response) {
+        return response.json()
+      }).then(function(json) {
+        assert.equal(json.name, 'Hubot')
+        assert.equal(json.login, 'hubot')
+      })
+    })
+
+    test('rejects json promise after body is consumed', function() {
+      return fetch('/json').then(function(response) {
+        assert(response.json, 'Body does not implement json')
+        response.json()
+        return response.json()
+      }).catch(function(error) {
+        assert(error instanceof TypeError, 'Promise rejected after body consumed')
+      })
+    })
+
+    test('handles json parse error', function() {
+      return fetch('/json-error').then(function(response) {
+        return response.json()
+      }).catch(function(error) {
+        assert(error instanceof Error, 'JSON exception is an Error instance')
+        assert(error.message, 'JSON exception has an error message')
+      })
+    })
+  })
+
+  suite('text', function() {
+    test('handles 204 No Content response', function() {
+      return fetch('/empty').then(function(response) {
+        assert.equal(response.status, 204)
+        return response.text()
+      }).then(function(body) {
+        assert.equal(body, '')
+      })
+    })
+
+    test('resolves text promise', function() {
+      return fetch('/hello').then(function(response) {
+        return response.text()
+      }).then(function(text) {
+        assert.equal(text, 'hi')
+      })
+    })
+
+    test('rejects text promise after body is consumed', function() {
+      return fetch('/hello').then(function(response) {
+        assert(response.text, 'Body does not implement text')
+        response.text()
+        return response.text()
+      }).catch(function(error) {
+        assert(error instanceof TypeError, 'Promise rejected after body consumed')
+      })
+    })
   })
 })
 
-suite('HTTP verbs', function() {
+// https://fetch.spec.whatwg.org/#methods
+suite('Methods', function() {
   test('supports HTTP GET', function() {
     return fetch('/request', {
       method: 'get',
@@ -215,8 +229,8 @@ suite('HTTP verbs', function() {
   })
 })
 
-// TODO: Avoid URL, not in PhantomJS
-suite('Redirect', function() {
+// https://fetch.spec.whatwg.org/#atomic-http-redirect-handling
+suite('Atomic HTTP redirect handling', function() {
   test('handles 301 redirect response', function() {
     return fetch('/redirect/301').then(function(response) {
       assert.equal(response.status, 200)
