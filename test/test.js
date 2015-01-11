cbd6c66fe4bbda1b63cef54c299c2081c4b50955
Joshua Peek <josh@joshpeek.com>
2015-01-10 17:14:16 -0800

    Switch to mocha

--- test/test.js
+++ test/test.js
@@ -1,13 +1,13 @@
-promiseTest('populates response body', 2, function() {
+test('populates response body', function() {
   return fetch('/hello').then(function(response) {
-    equal(response.status, 200)
+    assert.equal(response.status, 200)
     return response.text()
   }).then(function(body) {
-    equal(body, 'hi')
+    assert.equal(body, 'hi')
   })
 })
 
-promiseTest('sends request headers', 2, function() {
+test('sends request headers', function() {
   return fetch('/request', {
     headers: {
       'Accept': 'application/json',
@@ -16,236 +16,235 @@ promiseTest('sends request headers', 2, function() {
   }).then(function(response) {
     return response.json()
   }).then(function(json) {
-    equal(json.headers['accept'], 'application/json')
-    equal(json.headers['x-test'], '42')
+    assert.equal(json.headers['accept'], 'application/json')
+    assert.equal(json.headers['x-test'], '42')
   })
 })
 
-promiseTest('parses response headers', 2, function() {
+test('parses response headers', function() {
   return fetch('/headers?' + new Date().getTime()).then(function(response) {
-    equal(response.headers.get('Date'), 'Mon, 13 Oct 2014 21:02:27 GMT')
-    equal(response.headers.get('Content-Type'), 'text/html; charset=utf-8')
+    assert.equal(response.headers.get('Date'), 'Mon, 13 Oct 2014 21:02:27 GMT')
+    assert.equal(response.headers.get('Content-Type'), 'text/html; charset=utf-8')
   })
 })
 
-promiseTest('resolves promise on 500 error', 2, function() {
+test('resolves promise on 500 error', function() {
   return fetch('/boom').then(function(response) {
-    equal(response.status, 500)
+    assert.equal(response.status, 500)
     return response.text()
   }).then(function(body) {
-    equal(body, 'boom')
+    assert.equal(body, 'boom')
   })
 })
 
-promiseTest('rejects promise for network error', 1, function() {
+test('rejects promise for network error', function() {
   return fetch('/error').then(function(response) {
-    ok(false, 'HTTP status ' + response.status + ' was treated as success')
-    start()
+    assert(false, 'HTTP status ' + response.status + ' was treated as success')
   }).catch(function(error) {
-    ok(error instanceof TypeError, 'Rejected with Error')
+    assert(error instanceof TypeError, 'Rejected with Error')
   })
 })
 
-promiseTest('handles 204 No Content response', 2, function() {
+test('handles 204 No Content response', function() {
   return fetch('/empty').then(function(response) {
-    equal(response.status, 204)
+    assert.equal(response.status, 204)
     return response.text()
   }).then(function(body) {
-    equal(body, '')
+    assert.equal(body, '')
   })
 })
 
-promiseTest('resolves text promise', 1, function() {
+test('resolves text promise', function() {
   return fetch('/hello').then(function(response) {
     return response.text()
   }).then(function(text) {
-    equal(text, 'hi')
+    assert.equal(text, 'hi')
   })
 })
 
 if (Response.prototype.formData) {
-  promiseTest('parses form encoded response', 1, function() {
+  test('parses form encoded response', function() {
     return fetch('/form').then(function(response) {
       return response.formData()
     }).then(function(form) {
-      ok(form instanceof FormData, 'Parsed a FormData object')
+      assert(form instanceof FormData, 'Parsed a FormData object')
     })
   })
 }
 
-promiseTest('parses json response', 2, function() {
+test('parses json response', function() {
   return fetch('/json').then(function(response) {
     return response.json()
   }).then(function(json) {
-    equal(json.name, 'Hubot')
-    equal(json.login, 'hubot')
+    assert.equal(json.name, 'Hubot')
+    assert.equal(json.login, 'hubot')
   })
 })
 
-promiseTest('handles json parse error', 2, function() {
+test('handles json parse error', function() {
   return fetch('/json-error').then(function(response) {
     return response.json()
   }).catch(function(error) {
-    ok(error instanceof Error, 'JSON exception is an Error instance')
-    ok(error.message, 'JSON exception has an error message')
+    assert(error instanceof Error, 'JSON exception is an Error instance')
+    assert(error.message, 'JSON exception has an error message')
   })
 })
 
 if (Response.prototype.blob) {
-  promiseTest('resolves blob promise', 2, function() {
+  test('resolves blob promise', function() {
     return fetch('/hello').then(function(response) {
       return response.blob()
     }).then(function(blob) {
-      ok(blob instanceof Blob, 'blob is a Blob instance')
-      equal(blob.size, 2)
+      assert(blob instanceof Blob, 'blob is a Blob instance')
+      assert.equal(blob.size, 2)
     })
   })
 }
 
-promiseTest('post sets content-type header', 2, function() {
+test('post sets content-type header', function() {
   return fetch('/request', {
     method: 'post',
     body: new FormData()
   }).then(function(response) {
     return response.json()
   }).then(function(json) {
-    equal(json.method, 'POST')
-    ok(/^multipart\/form-data;/.test(json.headers['content-type']))
+    assert.equal(json.method, 'POST')
+    assert(/^multipart\/form-data;/.test(json.headers['content-type']))
   })
 })
 
 if (Response.prototype.blob) {
-  promiseTest('rejects blob promise after body is consumed', 2, function() {
+  test('rejects blob promise after body is consumed', function() {
     return fetch('/hello').then(function(response) {
-      ok(response.blob, 'Body does not implement blob')
+      assert(response.blob, 'Body does not implement blob')
       response.blob()
       return response.blob()
     }).catch(function(error) {
-      ok(error instanceof TypeError, 'Promise rejected after body consumed')
+      assert(error instanceof TypeError, 'Promise rejected after body consumed')
     })
   })
 }
 
-promiseTest('rejects json promise after body is consumed', 2, function() {
+test('rejects json promise after body is consumed', function() {
   return fetch('/json').then(function(response) {
-    ok(response.json, 'Body does not implement json')
+    assert(response.json, 'Body does not implement json')
     response.json()
     return response.json()
   }).catch(function(error) {
-    ok(error instanceof TypeError, 'Promise rejected after body consumed')
+    assert(error instanceof TypeError, 'Promise rejected after body consumed')
   })
 })
 
-promiseTest('rejects text promise after body is consumed', 2, function() {
+test('rejects text promise after body is consumed', function() {
   return fetch('/hello').then(function(response) {
-    ok(response.text, 'Body does not implement text')
+    assert(response.text, 'Body does not implement text')
     response.text()
     return response.text()
   }).catch(function(error) {
-    ok(error instanceof TypeError, 'Promise rejected after body consumed')
+    assert(error instanceof TypeError, 'Promise rejected after body consumed')
   })
 })
 
 if (Response.prototype.formData) {
-  promiseTest('rejects formData promise after body is consumed', 2, function() {
+  test('rejects formData promise after body is consumed', function() {
     return fetch('/json').then(function(response) {
-      ok(response.formData, 'Body does not implement formData')
+      assert(response.formData, 'Body does not implement formData')
       response.formData()
       return response.formData()
     }).catch(function(error) {
-      ok(error instanceof TypeError, 'Promise rejected after body consumed')
+      assert(error instanceof TypeError, 'Promise rejected after body consumed')
     })
   })
 }
 
-promiseTest('supports HTTP PUT', 2, function() {
+test('supports HTTP PUT', function() {
   return fetch('/request', {
     method: 'put',
     body: 'name=Hubot'
   }).then(function(response) {
     return response.json()
   }).then(function(request) {
-    equal(request.method, 'PUT')
-    equal(request.data, 'name=Hubot')
+    assert.equal(request.method, 'PUT')
+    assert.equal(request.data, 'name=Hubot')
   })
 })
 
-promiseTest('supports HTTP PATCH', 2, function() {
+test('supports HTTP PATCH', function() {
   return fetch('/request', {
     method: 'PATCH',
     body: 'name=Hubot'
   }).then(function(response) {
     return response.json()
   }).then(function(request) {
-    equal(request.method, 'PATCH')
+    assert.equal(request.method, 'PATCH')
     if (/PhantomJS/.test(navigator.userAgent)) {
-      equal(request.data, '')
+      assert.equal(request.data, '')
     } else {
-      equal(request.data, 'name=Hubot')
+      assert.equal(request.data, 'name=Hubot')
     }
   })
 })
 
-promiseTest('supports HTTP DELETE', 2, function() {
+test('supports HTTP DELETE', function() {
   return fetch('/request', {
     method: 'delete',
   }).then(function(response) {
     return response.json()
   }).then(function(request) {
-    equal(request.method, 'DELETE')
-    equal(request.data, '')
+    assert.equal(request.method, 'DELETE')
+    assert.equal(request.data, '')
   })
 })
 
-promiseTest('handles 301 redirect response', 3, function() {
+test('handles 301 redirect response', function() {
   return fetch('/redirect/301').then(function(response) {
-    equal(response.status, 200)
-    equal(response.url ? new URL(response.url).pathname : null, '/hello')
+    assert.equal(response.status, 200)
+    assert.equal(response.url ? new URL(response.url).pathname : null, '/hello')
     return response.text()
   }).then(function(body) {
-    equal(body, 'hi')
+    assert.equal(body, 'hi')
   })
 })
 
-promiseTest('handles 302 redirect response', 3, function() {
+test('handles 302 redirect response', function() {
   return fetch('/redirect/302').then(function(response) {
-    equal(response.status, 200)
-    equal(response.url ? new URL(response.url).pathname : null, '/hello')
+    assert.equal(response.status, 200)
+    assert.equal(response.url ? new URL(response.url).pathname : null, '/hello')
     return response.text()
   }).then(function(body) {
-    equal(body, 'hi')
+    assert.equal(body, 'hi')
   })
 })
 
-promiseTest('handles 303 redirect response', 3, function() {
+test('handles 303 redirect response', function() {
   return fetch('/redirect/303').then(function(response) {
-    equal(response.status, 200)
-    equal(response.url ? new URL(response.url).pathname : null, '/hello')
+    assert.equal(response.status, 200)
+    assert.equal(response.url ? new URL(response.url).pathname : null, '/hello')
     return response.text()
   }).then(function(body) {
-    equal(body, 'hi')
+    assert.equal(body, 'hi')
   })
 })
 
-promiseTest('handles 307 redirect response', 3, function() {
+test('handles 307 redirect response', function() {
   return fetch('/redirect/307').then(function(response) {
-    equal(response.status, 200)
-    equal(response.url ? new URL(response.url).pathname : null, '/hello')
+    assert.equal(response.status, 200)
+    assert.equal(response.url ? new URL(response.url).pathname : null, '/hello')
     return response.text()
   }).then(function(body) {
-    equal(body, 'hi')
+    assert.equal(body, 'hi')
   })
 })
 
 // PhantomJS doesn't support 308 redirects
 if (!navigator.userAgent.match(/PhantomJS/)) {
-  promiseTest('handles 308 redirect response', 3, function() {
+  test('handles 308 redirect response', function() {
     return fetch('/redirect/308').then(function(response) {
-      equal(response.status, 200)
-    equal(response.url ? new URL(response.url).pathname : null, '/hello')
+      assert.equal(response.status, 200)
+    assert.equal(response.url ? new URL(response.url).pathname : null, '/hello')
       return response.text()
     }).then(function(body) {
-      equal(body, 'hi')
+      assert.equal(body, 'hi')
     })
   })
 }
