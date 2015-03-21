89b449384fe7e1858615d2ecf85c78802815d7bb
Kirill Konshin <kirill.konshin@gmail.com>
2015-03-20 20:32:24 -0700

    Headers constructor in Response constructor Fix for #106

--- fetch.js
+++ fetch.js
@@ -327,7 +327,7 @@
     this.status = options.status
     this.ok = this.status >= 200 && this.status < 300
     this.statusText = options.statusText
-    this.headers = options.headers
+    this.headers = options.headers instanceof Headers ? options.headers : new Headers(options.headers)
     this.url = options.url || ''
   }
 
