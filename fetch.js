c5200ebe021d1ed0222d6005641ecac35e29e589
Atticus White <contact@atticuswhite.com>
2015-12-06 00:41:17 -0500

    added PATCH to normalized method names

--- fetch.js
+++ fetch.js
@@ -197,7 +197,7 @@
   }
 
   // HTTP methods whose capitalization should be normalized
-  var methods = ['DELETE', 'GET', 'HEAD', 'OPTIONS', 'POST', 'PUT']
+  var methods = ['DELETE', 'GET', 'HEAD', 'OPTIONS', 'POST', 'PUT', 'PATCH']
 
   function normalizeMethod(method) {
     var upcased = method.toUpperCase()
