b33cb7a08b6fa8539a7203769a55192eb2555d57
stelcheck <mtrudel@wizcorp.jp>
2015-12-11 12:07:07 +0900

    * Accept HTTP codes over 599

--- fetch.js
+++ fetch.js
@@ -342,7 +342,7 @@
 
       xhr.onload = function() {
         var status = (xhr.status === 1223) ? 204 : xhr.status
-        if (status < 100 || status > 599) {
+        if (status < 100) {
           reject(new TypeError('Network request failed'))
           return
         }
