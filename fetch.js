8deb8296681f6ad0990e0af47b99d71f2a1d1701
Steven Luscher <steveluscher@users.noreply.github.com>
2016-03-08 21:34:43 +0000

    Guard against `xhr.getAllResponseHeaders()` being `null`
    From https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest#getAllResponseHeaders():
    
    > Returns all the response headers, separated by CRLF, as a string, or null if no response has been received.
    
    I discovered this on Android, once when `xhr.getAllResponseHeaders()` evaluated to `null`.

--- fetch.js
+++ fetch.js
@@ -267,7 +267,7 @@
 
   function headers(xhr) {
     var head = new Headers()
-    var pairs = xhr.getAllResponseHeaders().trim().split('\n')
+    var pairs = (xhr.getAllResponseHeaders() || '').trim().split('\n')
     pairs.forEach(function(header) {
       var split = header.trim().split(':')
       var key = split.shift().trim()
