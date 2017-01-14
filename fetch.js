23a458df1fc8a3f422f2c45b8d2d42d1eed396a0
Yasutaka Kawamoto <kwmt27@gmail.com>
2017-01-14 11:37:38 +0900

    When I build by rollup, I got the bellow error.
    ⚠️   The 'this' keyword is equivalent to 'undefined' at the top level of an ES module, and has been rewritten
    https://github.com/rollup/rollup/wiki/Troubleshooting#this-is-undefined
    node_modules/whatwg-fetch/fetch.js (460:40)
    458:   }
    459:   self.fetch.polyfill = true
    460: })(typeof self !== 'undefined' ? self : this);
                                                 ^

--- fetch.js
+++ fetch.js
@@ -455,4 +455,6 @@
     })
   }
   self.fetch.polyfill = true
-})(typeof self !== 'undefined' ? self : this);
+    //The 'this' keyword is equivalent to 'undefined' at the top level of an ES module, and has been rewritten
+    //https://github.com/rollup/rollup/wiki/Troubleshooting#this-is-undefined
+})(typeof self !== 'undefined' ? self : options.context);
