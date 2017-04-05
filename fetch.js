b9660231e259580eb356ce82ef3acefd4014d773
Flandre Scarlet <i@2333.moe>
2017-04-05 19:13:01 +0800

    feat: allow `:` in headers
    Here's a request's header:
    
    ```
    :authority:jandan.net
    :method:GET
    :path:/ooxx
    :scheme:https
    accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
    
    ...
    ```
    
    see https://jandan.net/ooxx

--- fetch.js
+++ fetch.js
@@ -46,7 +46,7 @@
     if (typeof name !== 'string') {
       name = String(name)
     }
-    if (/[^a-z0-9\-#$%&'*+.\^_`|~]/i.test(name)) {
+    if (/[^a-z0-9\-#$%&'*+.\^_`|~\:]/i.test(name)) {
       throw new TypeError('Invalid character in header field name')
     }
     return name.toLowerCase()
