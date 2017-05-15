5f3ab00c4b5574e563f0845742d50387ee33582e
Cameron Bates <cameronb@marketamerica.com>
2017-05-15 14:47:12 -0400

    Remove toLowerCase of header name

--- fetch.js
+++ fetch.js
@@ -49,7 +49,7 @@
     if (/[^a-z0-9\-#$%&'*+.\^_`|~]/i.test(name)) {
       throw new TypeError('Invalid character in header field name')
     }
-    return name.toLowerCase()
+    return name
   }
 
   function normalizeValue(value) {
