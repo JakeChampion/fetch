074c89e2fc117f692b11c8b6b4fda55a330b600b
Mislav Marohnić <mislav.marohnic@gmail.com>
2017-12-13 15:27:13 +0100

    Polyfill DOMException
    In PhantomJS, DOMException exists but is not a constructor.

--- fetch.js
+++ fetch.js
@@ -417,10 +417,18 @@
     return new Response(null, {status: status, headers: {location: url}})
   }
 
-  var DOMException = self.DOMException || function(message, name) {
-    var error = new Error(message)
-    error.name = name
-    return error
+  var DOMException = self.DOMException
+  try {
+    new DOMException()
+  } catch(err) {
+    DOMException = function(message, name) {
+      this.message = message
+      this.name = name
+      var error = Error(message)
+      this.stack = error.stack
+    }
+    DOMException.prototype = Object.create(Error.prototype)
+    DOMException.prototype.constructor = DOMException
   }
 
   self.Headers = Headers
