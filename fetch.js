b65ed608604492d605df2d62cd4c5050e2a8d508
Jake Champion <me@jakechampion.name>
2020-07-09 16:23:53 +0100

    If headers are passed in via a Record then do not normalise the header names as part of the request

--- fetch.js
+++ fetch.js
@@ -547,9 +547,15 @@ export function fetch(input, init) {
       }
     }
 
-    request.headers.forEach(function(value, name) {
-      xhr.setRequestHeader(name, value)
-    })
+    if (init && typeof init.headers === 'object' && !(init.headers instanceof Headers)) {
+      Object.getOwnPropertyNames(init.headers).forEach(function(name) {
+        xhr.setRequestHeader(name, normalizeValue(init.headers[name]))
+      })
+    } else {
+      request.headers.forEach(function(value, name) {
+        xhr.setRequestHeader(name, value)
+      })
+    }
 
     if (request.signal) {
       request.signal.addEventListener('abort', abortXhr)
