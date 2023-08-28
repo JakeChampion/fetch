JakeChampion/fetch

commit 0c1d2b97b7edf636323534b13626a8bb9f5fe22d

validate status is in range fixes https://github.com/JakeChampion/fetch/issues/1213

fetch.js
@@ -462,6 +462,9 @@ export function Response(bodyInit, options) {
 
   this.type = 'default'
   this.status = options.status === undefined ? 200 : options.status
+  if (this.status < 200 || this.status > 599) {
+    throw new RangeError("Failed to construct 'Response': The status provided (0) is outside the range [200, 599].")
+  }
   this.ok = this.status >= 200 && this.status < 300
   this.statusText = options.statusText === undefined ? '' : '' + options.statusText
   this.headers = new Headers(options.headers)
@@ -481,7 +484,8 @@ Response.prototype.clone = function() {
 }
 
 Response.error = function() {
-  var response = new Response(null, {status: 0, statusText: ''})
+  var response = new Response(null, {status: 200, statusText: ''})
+  response.status = 0
   response.type = 'error'
   return response
 }

test/test.js
@@ -623,6 +623,21 @@ exercise.forEach(function(exerciseMode) {
           Response('{"foo":"bar"}', {headers: {'content-type': 'application/json'}})
         })
       })
+      test('status outside inclusive range 200-599 ', function() {
+        assert.throws(function() {
+          new Response('', {status: 199})
+        })
+        for (var i = 0; i < 200; i++) {
+          assert.throws(function() {
+            new Response('', {status: i})
+          })
+        }
+        for (i = 999; i > 599; i--) {
+          assert.throws(function() {
+            new Response('', {status: i})
+          })
+        }
+      })
       test('creates Headers object from raw headers', function() {
         var r = new Response('{"foo":"bar"}', {headers: {'content-type': 'application/json'}})
         assert.equal(r.headers instanceof Headers, true)

commit 96017dedebec6672c00441bfffc899d38864ce39

Fix - File fetching broken since commit 0c1d2b9 (#1375)

fetch.js
@@ -538,9 +538,15 @@ export function fetch(input, init) {
 
     xhr.onload = function() {
       var options = {
-        status: xhr.status,
         statusText: xhr.statusText,
         headers: parseHeaders(xhr.getAllResponseHeaders() || '')
+      }
+      // This check if specifically for when a user fetches a file locally from the file system
+      // Only if the status is out of a normal range
+      if (request.url.startsWith('file://') && (xhr.status < 200 || xhr.status > 599)) {
+        options.status = 200;
+      } else {
+        options.status = xhr.status;
       }
       options.url = 'responseURL' in xhr ? xhr.responseURL : options.headers.get('X-Request-URL')
       var body = 'response' in xhr ? xhr.response : xhr.responseText

test/test.js
@@ -647,7 +647,15 @@ exercise.forEach(function(exerciseMode) {
             new Response('', {status: i})
           })
         }
+        // A fetch with the url of a `file://` scheme may have a status 0 or
+        // similar in some operating systems. In the event that a status is found outside 
+        // the standard range of 200-599, and the url start with `file://`
+        // the status should return 200
+        assert.doesNotThrow(function() {
+          new Request('', {status: 0, request: {url: 'file://path/to/local/file'}})
+        })
       })
+
       test('creates Headers object from raw headers', function() {
         var r = new Response('{"foo":"bar"}', {headers: {'content-type': 'application/json'}})
         assert.equal(r.headers instanceof Headers, true)